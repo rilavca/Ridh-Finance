f00c732ee3012c44bc04bb853ef7f0423c979a02
rilavca <rilav.ca@gmail.com>
2026-04-12 17:49:09 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'rf-v13';
+const ASSETS = ['./index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate' || e.request.url.endsWith('index.html')) {
+    // Always fetch fresh HTML from network; cache only as offline fallback
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(cached =>
+        cached || fetch(e.request).then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        }).catch(() => cached)
+      )
+    );
+  }
+});
